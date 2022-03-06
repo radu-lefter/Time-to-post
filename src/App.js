6da489295c1125178d4c15d48df029cc5bfde440
radu-lefter <68811789+radu-lefter@users.noreply.github.com>
2022-03-06 21:25:17 +0000

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { Normalize } from 'styled-normalize';
+import GlobalStyle from './GlobalStyle';
+import theme from './theme';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+    <Router>
+      <Normalize />
+      <GlobalStyle />
+      <Routes>
+        <Route path="/search">Search</Route>
+        <Route path="/">Home</Route>
+        <Route path="*">404 - Not Found</Route>
+      </Routes>
+    </Router>
+  </ThemeProvider>
   );
 }
 
---
+++ src/GlobalStyle.js
@@ -0,0 +1,55 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  /** Montserrat Regular **/
+  @font-face {
+    font-family: "Montserrat";
+    font-weight: 400;
+    font-style: normal;
+    src: url("/fonts/Montserrat-Regular.woff2") format("woff2"),
+ url("/fonts/Montserrat-Regular.woff") format("woff");
+  }
+
+  /** Montserrat Medium **/
+  @font-face {
+    font-family: "Montserrat";
+    font-weight: 500;
+    font-style: normal;
+    src: url("/fonts/Montserrat-Medium.woff2") format("woff2"),
+ url("/fonts/Montserrat-Medium.woff") format("woff");
+  }
+
+  /** Montserrat SemiBold **/
+  @font-face {
+    font-family: "Montserrat";
+    font-weight: 600;
+    font-style: normal;
+    src: url("/fonts/Montserrat-SemiBold.woff2") format("woff2"),
+ url("/fonts/Montserrat-SemiBold.woff") format("woff");
+  }
+
+  /** Montserrat Bold **/
+  @font-face {
+    font-family: "Montserrat";
+    font-weight: 700;
+    font-style: normal;
+    src: url("Montserrat-Bold.woff2") format("woff2"),
+ url("Montserrat-Bold.woff") format("woff");
+  }
+
+  /** Lora Regular **/
+  @font-face {
+    font-family: "Lora";
+    src: url("/fonts/Lora-Regular.woff2") format("woff2"),
+ url("/fonts/Lora-Regular.woff") format("woff");
+  }
+
+  body {
+    font-family: ${(props) => props.theme.font.family.default};
+    font-size: ${(props) => props.theme.font.size.default};
+    line-height: ${(props) => props.theme.font.lineHeight.default};
+    color: ${(props) => props.theme.color.text};
+  }
+`;
+
+export default GlobalStyle;
